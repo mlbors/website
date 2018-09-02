3a5040e2c56e0c674c14f6e58539b87d8e995b51
lancelot <matyas.bors@gmail.com>
2018-09-02 11:52:14 +0200

    Add unit tests

--- src/app/site/components/image-viewer-wrapper/image-viewer-wrapper.component.spec.ts
+++ src/app/site/components/image-viewer-wrapper/image-viewer-wrapper.component.spec.ts
@@ -11,17 +11,55 @@
 /********** IMPORTS **********/
 /*****************************/
 
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 import { RouterTestingModule} from '@angular/router/testing';
 import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 import { DebugElement } from '@angular/core';
 import { By } from '@angular/platform-browser';
 
+import { Router, Routes } from '@angular/router';
+import { Component, Input } from '@angular/core';
+
 import { ImageViewerWrapperComponent } from './image-viewer-wrapper.component';
 
 /********************************************************************************/
 /********************************************************************************/
+
+/*********************************************/
+/********** APP POST COMPONENT MOCK **********/
+/*********************************************/
+
+@Component({
+  selector: 'app-site-component',
+  template: ''
+})
+class MockSiteComponent {
+}
+
+@Component({
+  selector: 'app-image-viewer',
+  template: ''
+})
+class MockImageViewerComponent {
+}
+
+
+/********************************************************************************/
+/********************************************************************************/
+
+const MockRoutes: Routes = [
+  {
+    path: '',
+    component: MockSiteComponent,
+    children: [
+      {
+        path: 'image-viewer/:img/:previous',
+        component: MockImageViewerComponent
+      }
+    ]
+  }
+];
 
 /****************************/
 /********** SET UP **********/
@@ -43,10 +81,12 @@ describe('ImageViewerWrapperComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        ImageViewerWrapperComponent
+        ImageViewerWrapperComponent,
+        MockSiteComponent,
+        MockImageViewerComponent
       ],
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes(MockRoutes),
         NoopAnimationsModule
       ]
     })
@@ -105,5 +145,24 @@ describe('ImageViewerWrapperComponent', () => {
     debugElement = fixture.debugElement.query(By.css('.image-viewer-wrapper'));
     expect(debugElement).toBeNull();
   });
+
+  /********************************************************************************/
+  /********************************************************************************/
+
+  /*************************************/
+  /********** SHOULD NAVIGATE **********/
+  /*************************************/
+
+  it('should navigate', fakeAsync(() => {
+    component.imageUrl = 'http://foo-url/img.jpg';
+    component.currentUrl = 'http://foo-current-url';
+    const router = TestBed.get(Router);
+    fixture.detectChanges();
+    component.enlarge();
+    tick();
+    const re = new RegExp('%2F', 'g');
+    const result = router.url.replace(re, '/');
+    expect(result).toBe('/image-viewer/http://foo-url/img.jpg/http://foo-current-url');
+  }));
 });
 
