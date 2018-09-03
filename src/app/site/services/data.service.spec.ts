9c2be085adb6ca7e92c2d388bb8f18df2a141f15
lancelot <matyas.bors@gmail.com>
2018-09-03 20:05:12 +0200

    Add tests

--- src/app/site/services/data.service.spec.ts
+++ src/app/site/services/data.service.spec.ts
@@ -13,7 +13,20 @@
 
 import { TestBed, inject } from '@angular/core/testing';
 
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+
+import { IMenu } from '../interfaces/imenu';
+import { IMetaData } from '../interfaces/imeta-data';
+import { IPost } from '../interfaces/ipost';
+import { IPostService } from '../interfaces/ipost-service';
+import { IQueryable} from '../interfaces/iqueryable';
+import { IQueryService } from '../interfaces/iquery-service';
+import { ITaxonomy } from '../interfaces/itaxonomy';
+import { ITaxonomyService } from '../interfaces/itaxonomy-service';
+import { ITerm} from '../interfaces/iterm';
+import { ITermService } from '../interfaces/iterm-service';
+import { IType } from '../interfaces/itype';
+import { IWebData } from '../interfaces/iweb-data';
 
 import { DataService } from './data.service';
 
@@ -34,6 +47,8 @@ describe('DataService', () => {
   /*********************************/
 
   beforeEach(() => {
+    let httpMock: HttpTestingController;
+
     TestBed.configureTestingModule({
       providers: [
         DataService
@@ -42,6 +57,8 @@ describe('DataService', () => {
         HttpClientTestingModule
       ]
     });
+
+    httpMock = TestBed.get(HttpTestingController);
   });
 
   /********************************************************************************/
@@ -53,5 +70,37 @@ describe('DataService', () => {
 
   it('should be created', inject([DataService], (service: DataService) => {
     expect(service).toBeTruthy();
+  }));
+
+  /********************************************************************************/
+  /********************************************************************************/
+
+  /*****************************************/
+  /********** SHOULD RECEIVE DATA **********/
+  /*****************************************/
+
+  it('should receive data', inject([HttpTestingController, DataService], (httpMock: HttpTestingController, service: DataService) => {
+    service.getData().subscribe(result => {
+      expect(result.navigationData).toBeTruthy();
+      expect(result.navigationData.length).toEqual(1);
+      expect((result.navigationData[0] as IMenu).id).toEqual('foo-menu');
+    });
+
+    const menuReq = httpMock.expectOne('assets/data/json/menus.json');
+    expect(menuReq.request.method).toEqual('GET');
+
+    const navItemsReq = httpMock.expectOne('assets/data/json/navigation-items.json');
+    expect(navItemsReq.request.method).toEqual('GET');
+
+    const mockMenuResponse = [
+      {
+        id: 'foo-menu',
+        slug: 'main-menu',
+        name: 'Main menu'
+      }
+    ];
+
+    menuReq.flush(mockMenuResponse);
+    httpMock.verify();
   }));
 });
